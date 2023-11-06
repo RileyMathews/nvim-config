7feea31a354ebb65b36779b48dd23258c72dee59
Riley Mathews <rileymathews80@gmail.com>
2023-11-05 21:39:06 -0600

    relative numbers

--- lua/rileymathews/telescopesetup.lua
+++ lua/rileymathews/telescopesetup.lua
@@ -31,7 +31,7 @@ vim.keymap.set('n', '<leader>/', function()
   })
 end, { desc = '[/] Fuzzily search in current buffer' })
 
-vim.keymap.set('n', '<leader>sf', require("telescope.builtin").find_files, { desc = '[S]earch [F]iles' }) 
+vim.keymap.set('n', '<leader>sf', require("telescope.builtin").find_files, { desc = '[S]earch [F]iles' })
 vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
 vim.keymap.set('n', '<leader>af', find_all_including_hidden_and_gitignored, { desc = '[A]ll [F]iles' })
 vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })

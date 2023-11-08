6ba8f45423a744d4ae9c62216799723d9e6ecd3b
Riley Mathews <rileymathews80@gmail.com>
2023-11-07 20:43:34 -0600

    added keymaps

--- lua/rileymathews/remap.lua
+++ lua/rileymathews/remap.lua
@@ -1,2 +1,32 @@
-vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- half page jumping
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- keep cursor in middle when searching
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- optional paste that does not put replaced text into buffer
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
+-- optional yank into system clipboard
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
+
+-- delete into the void
+vim.keymap.set("n", "<leader>d", "\"_d")
+vim.keymap.set("v", "<leader>d", "\"_d")
+
+-- disable Q
+vim.keymap.set("n", "Q", "<nop>")
+
+-- make current file executable
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>")

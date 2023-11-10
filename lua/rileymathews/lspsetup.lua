7177200656721b8e351abcd288b12cd438b128ce
Riley Mathews <rileymathews80@gmail.com>
2023-11-10 14:29:35 -0600

    added auto pairing

--- lua/rileymathews/lspsetup.lua
+++ lua/rileymathews/lspsetup.lua
@@ -109,7 +109,11 @@ local cmp = require 'cmp'
 local luasnip = require 'luasnip'
 require('luasnip.loaders.from_vscode').lazy_load()
 luasnip.config.setup {}
-
+local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+cmp.event:on(
+  'confirm_done',
+  cmp_autopairs.on_confirm_done()
+)
 cmp.setup {
     snippet = {
         expand = function(args)
